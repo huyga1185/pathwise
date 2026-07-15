bcf935fb8e5b9b865fa83fa6d2134d79b1f72836
huyga1185 <huylay1054@gmail.com>
2026-07-16 02:17:00 +0700

    chore: rebrand project to PathWise and wire up MapStruct
    - Rename Maven artifact `be` -> `pathwise` and drop empty POM metadata
    - Add MapStruct + lombok-mapstruct-binding annotation processing paths
    - Rename ToApplication -> PathWiseApplication
    - Update API user-agent to PathWise and pin Hibernate JDBC time zone to UTC

--- be/src/main/java/uk/huy/pathwise/PathWiseApplication.java
+++ be/src/main/java/uk/huy/pathwise/PathWiseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToApplication {
+public class PathWiseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToApplication.class, args);
+		SpringApplication.run(PathWiseApplication.class, args);
 	}
 
 }
