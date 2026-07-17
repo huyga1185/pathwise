889e4add6f50639bfddbca0a766626f94e9fb23f
huyga1185 <huylay1054@gmail.com>
2026-07-17 11:47:09 +0700

    refactor(user): move password encoder bean to PasswordEncoderConfig

--- be/src/main/java/uk/huy/pathwise/user/config/PasswordEncoderConfig.java
+++ be/src/main/java/uk/huy/pathwise/user/config/PasswordEncoderConfig.java
@@ -1,4 +1,4 @@
-package uk.huy.pathwise.shared.config;
+package uk.huy.pathwise.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class PasswordEncoderConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
